a510cc1147cd888f651d52712c2270be2c7437d2
Betty Rodriguez <blrodriguez3@espe.edu.ec>
2024-12-17 13:19:15 -0500

    Me que de en la seccion  coneccion con la base de datos

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+var connectionString= builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<MyIdentityDBContext>(o => o.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
